8dc4da73f7e66c26c972bc6d66ba93da423189e4
Charisma <actuallycharisma@outlook.com>
2024-06-27 17:42:34 +0100

    fixed the empty search bar issue

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,29 @@ function App() {
     }
 ]); 
 
+const [cakesStatic, setCakesStatic] = useState([
+  {
+      cakeName: "Lemon Drizzle",
+      ingredients: ["eggs", "butter", "lemon  zest", "sugar", "self-raising flour"],
+      rating: 5
+  },
+  {
+      cakeName: "Tea Loaf",
+      ingredients: ["eggs", "oil", "dried fruit", "sugar", "self-raising flour", "strong tea"],
+      rating: 3
+  },
+  {
+      cakeName: "Brownie",
+      ingredients: ["chocolate", "eggs", "flour", "butter", "walnuts"],
+      rating: 4
+  },
+  {
+      cakeName: "Carrot Cake",
+      ingredients: ["carrots", "walnuts", "oil", "cream cheese", "flour", "sugar"],
+      rating: 5
+  }
+]); 
+
 
   return (
     <>
@@ -39,7 +62,7 @@ function App() {
       </header>
       <main>
         <RecipeList cakes={cakes} setCakes={setCakes}/>
-        <Search cakes={cakes} setCakes={setCakes}/>
+        <Search cakes={cakes} setCakes={setCakes} cakesStatic={cakesStatic} setCakesStatic={setCakesStatic}/>
         <CakeForm cakes={cakes} setCakes={setCakes}/>
       </main>
     </>
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,25 +1,32 @@
 import React, {useState} from 'react'; 
 //the search component
-const Search = ({cakes, setCakes}) => {
+const Search = ({cakes, setCakes, cakesStatic, setCakesStatic}) => {
 
-    const [searchCake, setSearchCake] = useState(''); 
+    const [searchCake, setSearchCake] = useState(""); 
+
     const filterCakes = (searchCake) => {
         console.log(searchCake); 
-        const filteredCakes = cakes.filter(cake => cake.cakeName.toLowerCase().includes(searchCake.toLowerCase())); 
-        return filteredCakes; 
+        if(searchCake.trim() === ""){
+            setCakes(cakesStatic);
+        } else{
+            const filteredCakes = cakes.filter(cake => 
+                cake.cakeName.toLowerCase().includes(searchCake.toLowerCase())
+            ); 
+            setCakes(filteredCakes);         
+        }
+        
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const filteredCakes = filterCakes(searchCake); 
-        setCakes(filteredCakes); 
+        filterCakes(searchCake); 
 
     }
 
     return(
         <form onSubmit={handleSubmit}>
             <label htmlFor="search-field">Search for a cake:</label>
-            <input type="text" id="search-field" placeholder='Search for a cake' onChange={(e) => setSearchCake(e.target.value)}/>
+            <input type="text" id="search-field" placeholder='Search for a cake' value={searchCake} onChange={(e) => setSearchCake(e.target.value)}/>
             <input type="submit"/>
         </form>
     )
